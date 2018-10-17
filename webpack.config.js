f19a1f0e654b7ddef4c85ab1c2b072ce2545cd6f
NathanDePachtere <de.pachtere.nat@gmail.com>
2018-10-17 16:54:31 +0200

    Initial configuration of Webpack Encore

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var Encore = require('@symfony/webpack-encore');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+Encore
+// the project directory where all compiled assets will be stored
+    .setOutputPath('public/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    // will create public/build/app.js and public/build/app.css
+    .createSharedEntry('layout', './assets/js/layout.js')
+    .addEntry('homepage', './assets/js/homepage.js')
+
+
+    .enableBuildNotifications()
+    .autoProvidejQuery()
+    .addPlugin(new CopyWebpackPlugin([
+        {from: './assets/static', to: 'static'}
+    ]))
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
+    .cleanupOutputBeforeBuild()
+    .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
